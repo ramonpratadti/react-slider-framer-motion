da8ef85f477ab2a6d2fc433e72f0084da9d09bfb
ramonpratadti <43249169+ramonpratadti@users.noreply.github.com>
2023-05-15 19:14:50 -0300

    1 file modified

--- SwipperContainer.tsx
+++ SwipperContainer.tsx
@@ -4,20 +4,41 @@ import './style.css';
 import { images } from './images';
 
 export default function SwipperContainer() {
-  const [imagesRender, setImagesRender] = React.useState(images.reverse());
+  const [imagesRender, setImagesRender] = React.useState(images);
   const containerRef = React.useRef(null);
-  const thresholdRight = window.innerWidth * 0.15;
-  const thresholdLeft = window.innerWidth * 0.15;
+  const thresholdRight = window.innerWidth * 0.1;
+  const thresholdLeft = window.innerWidth * 0.1;
+
+  const [prev, setPrev] = React.useState(-1);
+  const [current, setCurrent] = React.useState(0);
+  const [next, setNext] = React.useState(1);
+
+  const swippedImages = [];
 
   const handleDragEnd = (event, info) => {
     if (info.offset.x > thresholdRight) {
       // Swiped right
-      // Perform action or transition to next card
-      console.log('Swiped right');
+      setTimeout(() => {
+        if (current > 0) {
+          setPrev(current - 2);
+          setCurrent(current - 1);
+          setNext(current);
+          console.log('Swiped right');
+        }
+      }, 1000);
     } else if (info.offset.x < -thresholdLeft) {
+      if (current < images.length - 1) {
+        setTimeout(() => {
+          if (current < images.length) {
+            setPrev(current);
+            setCurrent(current + 1);
+            setNext(current + 2);
+            console.log('Swiped left');
+          }
+        }, 1000);
+      }
       // Swiped left
       // Perform action or transition to previous card
-      console.log('Swiped left');
     } else {
       // Card is not swiped far enough, animate back to initial position
       // motion.animate(dragX, 0, { type: 'spring' });
@@ -26,26 +47,21 @@ export default function SwipperContainer() {
 
   return (
     <div style={{ width: '100vw', height: '100vh', position: 'absolute' }}>
-      {imagesRender.map((i, idx) => (
-        <motion.div
-          onDragEnd={handleDragEnd}
-          key={i.src}
-          drag="x"
-          dragConstraints={{
-            left: idx === 0 ? 0 : -window.innerWidth,
-            right: idx === images.length - 1 ? 0 : window.innerWidth,
-          }}
-          className="item"
-          style={{
-            zIndex: idx,
-          }}
-          transition={{
-            duration: 0.5,
-          }}
-        >
-          <img src={i.src} />
-        </motion.div>
-      ))}
+      <motion.div
+        onDragEnd={handleDragEnd}
+        key={imagesRender[current].src}
+        drag="x"
+        dragConstraints={{
+          left: current < images.length - 1 ? -412 : 0,
+          right: current > 0 ? 412 : 0,
+        }}
+        className="item"
+        style={{
+          zIndex: 1,
+        }}
+      >
+        <img src={imagesRender[current].src} />
+      </motion.div>
     </div>
   );
 }
